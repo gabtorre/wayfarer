8603da0fd5b04a51b660ff0282bd37f55c165916
gabtorre <gtorresx3@gmail.com>
2020-10-14 14:11:05 -0700

    Deployment

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_fields import DefaultStaticImageField
 from django.core.files.storage import FileSystemStorage
 fs=FileSystemStorage(location='media/images')
 from django.utils import timezone
